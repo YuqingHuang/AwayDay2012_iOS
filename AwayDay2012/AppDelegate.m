4c6377968f748db430d7d19ab6b24ea400c2bb82
account-01\d685156 <Yuqing.Huang@team.telstra.com>
2013-08-10 15:15:34 +0800

    [Yuqing] add urban airship to support APNs.

--- AwayDay2012/AppDelegate.m
+++ AwayDay2012/AppDelegate.m
@@ -11,6 +11,10 @@
 #import "AppConstant.h"
 #import "DBService.h"
 
+#import "UAirship.h"
+#import "UAConfig.h"
+#import "UAPush.h"
+
 #define away_day_user_state_key @"away_day_2012_user_state"
 #define away_day_user_db_name   @"user_db.sqlite"
 
@@ -30,6 +34,14 @@
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    UAConfig *config = [UAConfig defaultConfig];
+    // You can also programatically override the plist values:
+    // config.developmentAppKey = @"YourKey";
+    // etc.
+    
+    // Call takeOff (which creates the UAirship singleton)
+    [UAirship takeOff:config];
+    
     [WeiboSDK enableDebugMode:YES];
     [WeiboSDK registerApp:kAppKey];
 
